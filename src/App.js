4063daf74fb398122a40c1da86fe6f7cfa0dbb47
Cassy Blaise <cassyblaise@gmail.com>
2020-03-09 12:35:16 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -76,17 +76,17 @@ onInputChange =(event)=>{
 
 onButtonSubmit =()=>{
   this.setState({imageUrl: this.state.input});
-    fetch("http://localhost:3001/image", {
+    fetch("https://intense-bayou-26762.herokuapp.com/imageurl", {
       method: "post",
       headers: {"Content-Type": "application/json" },
       body: JSON.stringify({
-        id: this.state.input
+        input: this.state.input
       })
     })
     .then(response => response.json())
     .then(response =>{
       if(response){
-        fetch("http://localhost:3001/image", {
+        fetch("https://intense-bayou-26762.herokuapp.com/image", {
       method: "put",
       headers: {"Content-Type": "application/json" },
       body: JSON.stringify({
